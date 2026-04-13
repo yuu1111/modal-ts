da52384162863fb6c452a7f207080fb3f0b2c2ac
yuu1111 <yuu210yuu@gmail.com>
2026-04-14 00:34:13 +0900

    Simplify cloud_bucket_mount: use InvalidError, options object, remove redundant assertions

--- src/services/cloud_bucket_mount/cloud_bucket_mount.ts
+++ src/services/cloud_bucket_mount/cloud_bucket_mount.ts
@@ -1,4 +1,5 @@
 import type { ModalClient } from "@/core/client";
+import { InvalidError } from "@/core/errors";
 import {
 	CloudBucketMount_BucketType,
 	CloudBucketMount as CloudBucketMountProto,
@@ -40,7 +41,6 @@ export class CloudBucketMountService {
 			} else if (url.hostname.endsWith("storage.googleapis.com")) {
 				bucketType = CloudBucketMount_BucketType.GCP;
 			} else {
-				bucketType = CloudBucketMount_BucketType.S3;
 				this.#client.logger.debug(
 					"CloudBucketMount received unrecognized bucket endpoint URL. " +
 						"Assuming AWS S3 configuration as fallback.",
@@ -51,25 +51,27 @@ export class CloudBucketMountService {
 		}
 
 		if (params.requesterPays && !params.secret) {
-			throw new Error("Credentials required in order to use Requester Pays.");
+			throw new InvalidError(
+				"Credentials required in order to use Requester Pays.",
+			);
 		}
 
 		if (params.keyPrefix && !params.keyPrefix.endsWith("/")) {
-			throw new Error(
+			throw new InvalidError(
 				"keyPrefix will be prefixed to all object paths, so it must end in a '/'",
 			);
 		}
 
-		return new CloudBucketMount(
+		return new CloudBucketMount({
 			bucketName,
-			params.secret,
-			params.readOnly ?? false,
-			params.requesterPays ?? false,
-			params.bucketEndpointUrl,
-			params.keyPrefix,
-			params.oidcAuthRoleArn,
+			secret: params.secret,
+			readOnly: params.readOnly ?? false,
+			requesterPays: params.requesterPays ?? false,
+			bucketEndpointUrl: params.bucketEndpointUrl,
+			keyPrefix: params.keyPrefix,
+			oidcAuthRoleArn: params.oidcAuthRoleArn,
 			bucketType,
-		);
+		});
 	}
 }
 
@@ -77,35 +79,36 @@ export class CloudBucketMountService {
  * @description Modal Function 内からクラウドストレージバケットにアクセスするためのマウント
  */
 export class CloudBucketMount {
-	readonly bucketName!: string;
+	readonly bucketName: string;
 	readonly secret?: Secret;
-	readonly readOnly!: boolean;
-	readonly requesterPays!: boolean;
+	readonly readOnly: boolean;
+	readonly requesterPays: boolean;
 	readonly bucketEndpointUrl?: string;
 	readonly keyPrefix?: string;
 	readonly oidcAuthRoleArn?: string;
-	readonly #bucketType!: CloudBucketMount_BucketType;
+	readonly #bucketType: CloudBucketMount_BucketType;
 
 	/** @internal */
-	constructor(
-		bucketName: string,
-		secret: Secret | undefined,
-		readOnly: boolean,
-		requesterPays: boolean,
-		bucketEndpointUrl: string | undefined,
-		keyPrefix: string | undefined,
-		oidcAuthRoleArn: string | undefined,
-		bucketType: CloudBucketMount_BucketType,
-	) {
-		this.bucketName = bucketName;
-		if (secret !== undefined) this.secret = secret;
-		this.readOnly = readOnly;
-		this.requesterPays = requesterPays;
-		if (bucketEndpointUrl !== undefined)
-			this.bucketEndpointUrl = bucketEndpointUrl;
-		if (keyPrefix !== undefined) this.keyPrefix = keyPrefix;
-		if (oidcAuthRoleArn !== undefined) this.oidcAuthRoleArn = oidcAuthRoleArn;
-		this.#bucketType = bucketType;
+	constructor(opts: {
+		bucketName: string;
+		secret: Secret | undefined;
+		readOnly: boolean;
+		requesterPays: boolean;
+		bucketEndpointUrl: string | undefined;
+		keyPrefix: string | undefined;
+		oidcAuthRoleArn: string | undefined;
+		bucketType: CloudBucketMount_BucketType;
+	}) {
+		this.bucketName = opts.bucketName;
+		if (opts.secret !== undefined) this.secret = opts.secret;
+		this.readOnly = opts.readOnly;
+		this.requesterPays = opts.requesterPays;
+		if (opts.bucketEndpointUrl !== undefined)
+			this.bucketEndpointUrl = opts.bucketEndpointUrl;
+		if (opts.keyPrefix !== undefined) this.keyPrefix = opts.keyPrefix;
+		if (opts.oidcAuthRoleArn !== undefined)
+			this.oidcAuthRoleArn = opts.oidcAuthRoleArn;
+		this.#bucketType = opts.bucketType;
 	}
 
 	/** @internal */
